91414540438ec7414fb49adb9f6f7f7133c75dee
Matthew Carlson <matthewfcarlson@users.noreply.github.com>
2025-08-12 11:01:47 -0500

    audio

---
+++ download_audio.sh
@@ -0,0 +1,7 @@
+# ./yt-dlp_macos --audio-quality 0 --extract-audio -f ba -o "music/%(title)s.%(ext)s" 
+
+./yt-dlp_macos --audio-quality 0 --extract-audio -f ba -o "music/%(title)s.%(ext)s" https://www.youtube.com/watch?v=VRz2Zrw1St8
+./yt-dlp_macos --audio-quality 0 --extract-audio -f ba -o "music/%(title)s.%(ext)s" https://www.youtube.com/watch?v=GFM8mY5dySc
+./yt-dlp_macos --audio-quality 0 --extract-audio -f ba -o "music/%(title)s.%(ext)s" https://www.youtube.com/watch?v=jlkuQIRqY7o
+./yt-dlp_macos --audio-quality 0 --extract-audio -f ba -o "music/%(title)s.%(ext)s" https://www.youtube.com/watch?v=XCM73vcp1s4
+./yt-dlp_macos --audio-quality 0 --extract-audio -f ba -o "music/%(title)s.%(ext)s" https://www.youtube.com/watch?v=8IL6YxDbY34&pp=ygUTaG92dmR5IHdhcm5pbmcgc2lnbg%3D%3D
